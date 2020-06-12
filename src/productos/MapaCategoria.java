5b35a02aed14ec025654c2399e070986f5ac9c3a
Alan Perez <alanperezmdp@hotmail.com>
2020-06-12 15:07:33 -0300

    Error MapaCategorias

--- src/productos/MapaCategoria.java
+++ src/productos/MapaCategoria.java
@@ -7,7 +7,7 @@ import Colecciones.mapaGenerico;
 
 public class MapaCategoria extends mapaGenerico<Long, MapaProductos>{
 
-	mapaGenerico<Long, MapaProductos> mapa;
+//	mapaGenerico<Long, MapaProductos> mapa;
 	
 	public MapaCategoria() 
 	{
@@ -18,7 +18,7 @@ public class MapaCategoria extends mapaGenerico<Long, MapaProductos>{
 	{
 		if(existencia(idCategoria))
 		{
-			MapaProductos nuevo = mapa.buscar(idCategoria);
+			MapaProductos nuevo = buscar(idCategoria);
 			Iterator<Entry<Long, Producto>> it = nuevo.getIterator();
 			while(it.hasNext())
 			{
@@ -27,37 +27,37 @@ public class MapaCategoria extends mapaGenerico<Long, MapaProductos>{
 			}
 		} else {
 			
-			mapa.agregar(idCategoria, mapaNuevo);
+			agregar(idCategoria, mapaNuevo);
 		}
 	}
 	
 	public boolean existencia(Long idCategoria)
 	{
-		return mapa.existencia(idCategoria);
+		return existencia(idCategoria);
 	}
 	
 	public void eliminarProducto(Long idCategoria, Long idProducto)
 	{
-		MapaProductos nuevo = mapa.buscar(idCategoria);
+		MapaProductos nuevo = buscar(idCategoria);
 		nuevo.eliminar(idProducto);
 		
 	}
 	
 	public boolean existeProducto(Long idCategoria, Long idProducto)
 	{
-		MapaProductos nuevo = mapa.buscar(idCategoria);
+		MapaProductos nuevo = buscar(idCategoria);
 		return nuevo.existencia(idProducto);
 	}
 	
 	public Producto buscarProducto(Long idCategoria, Long idProducto)
 	{
-		MapaProductos nuevo = mapa.buscar(idCategoria);
+		MapaProductos nuevo = buscar(idCategoria);
 		return nuevo.buscar(idProducto);
 	}
 	
 	public String listarCategoriaProducto(Long idCategoria)
 	{
-		MapaProductos nuevo = mapa.buscar(idCategoria);
+		MapaProductos nuevo = buscar(idCategoria);
 		return nuevo.listar();
 	}
 }
