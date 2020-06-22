8a991a4febd5598d2c4e9fdf41429cb8033ee917
Alan Perez <alanperezmdp@hotmail.com>
2020-06-21 22:56:55 -0300

    Desarrollo de las clases Graficas

--- src/graficas/Cuenta.java
+++ src/graficas/Cuenta.java
@@ -18,6 +18,7 @@ import java.awt.Font;
 import javax.swing.JPasswordField;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionAdapter;
 import java.awt.Cursor;
 import javax.swing.SwingConstants;
@@ -39,6 +40,14 @@ public class Cuenta extends JFrame {
 	private JTextField fieldIngresar;
 	private JLabel lblNewLabel;
 	int x, y;
+	private JTextField fieldNombre;
+	private JTextField fieldApellido;
+	private JTextField fieldEdad;
+	private JTextField fieldTelefono;
+	private JLabel editarNombre;
+	private JLabel editarApellido;
+	private JLabel editarEdad;
+	private JLabel editarTelefono;
 	
 //	public static void main(String[] args) 
 //	{
@@ -58,7 +67,7 @@ public class Cuenta extends JFrame {
 	public Cuenta(Supermercado mercado) 
 	{
 		
-		Usuario user = mercado.getUsuarioEnSesion();
+		Usuario user = new Usuario("AlanGmer", "123", "Alan", "Perez", 22, "123456");
 		
 		setUndecorated(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -159,6 +168,61 @@ public class Cuenta extends JFrame {
 		botonHistorial.setBounds(50, 347, 196, 51);
 		contentPane.add(botonHistorial);
 		
+		fieldTelefono = new JTextField();
+		fieldTelefono.setText("Alan");
+		fieldTelefono.setOpaque(false);
+		fieldTelefono.setForeground(new Color(102, 102, 102));
+		fieldTelefono.setFont(new Font("Calibri", Font.PLAIN, 23));
+		fieldTelefono.setEditable(false);
+		fieldTelefono.setColumns(10);
+		fieldTelefono.setCaretColor(Color.BLACK);
+		fieldTelefono.setBorder(null);
+		fieldTelefono.setBackground(Color.WHITE);
+		fieldTelefono.setBounds(317, 641, 400, 29);
+		fieldTelefono.setText(user.getTelefono());
+		contentPane.add(fieldTelefono);
+		
+		fieldEdad = new JTextField();
+		fieldEdad.setText("Alan");
+		fieldEdad.setOpaque(false);
+		fieldEdad.setForeground(new Color(102, 102, 102));
+		fieldEdad.setFont(new Font("Calibri", Font.PLAIN, 23));
+		fieldEdad.setEditable(false);
+		fieldEdad.setColumns(10);
+		fieldEdad.setCaretColor(Color.BLACK);
+		fieldEdad.setBorder(null);
+		fieldEdad.setBackground(Color.WHITE);
+		fieldEdad.setBounds(317, 538, 400, 29);
+		fieldEdad.setText(String.valueOf(user.getEdad()));
+		contentPane.add(fieldEdad);
+		
+		fieldApellido = new JTextField();
+		fieldApellido.setText("Alan");
+		fieldApellido.setOpaque(false);
+		fieldApellido.setForeground(new Color(102, 102, 102));
+		fieldApellido.setFont(new Font("Calibri", Font.PLAIN, 23));
+		fieldApellido.setEditable(false);
+		fieldApellido.setColumns(10);
+		fieldApellido.setCaretColor(Color.BLACK);
+		fieldApellido.setBorder(null);
+		fieldApellido.setBackground(Color.WHITE);
+		fieldApellido.setBounds(318, 435, 400, 29);
+		fieldApellido.setText(user.getApellido());
+		contentPane.add(fieldApellido);
+		
+		fieldNombre = new JTextField();
+		fieldNombre.setOpaque(false);
+		fieldNombre.setForeground(new Color(102, 102, 102));
+		fieldNombre.setFont(new Font("Calibri", Font.PLAIN, 23));
+		fieldNombre.setEditable(false);
+		fieldNombre.setColumns(10);
+		fieldNombre.setCaretColor(Color.BLACK);
+		fieldNombre.setBorder(null);
+		fieldNombre.setBackground(Color.WHITE);
+		fieldNombre.setBounds(318, 334, 400, 29);
+		fieldNombre.setText(user.getNombre());
+		contentPane.add(fieldNombre);
+		
 		
 		fieldUsuario = new JTextField();
 		fieldUsuario.setCaretColor(new Color(0, 0, 0));
@@ -188,29 +252,41 @@ public class Cuenta extends JFrame {
 		
 		editarNombreUsuario = new JLabel("");
 		editarNombreUsuario.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		editarNombreUsuario.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent arg0) 
-			{
-				fieldUsuario.setEditable(true);			
-				fieldUsuario.selectAll();
-			}
-		});
+		editarNombreUsuario.addMouseListener(new EditarField(fieldUsuario));
 		
 		editarContraseña = new JLabel("");
 		editarContraseña.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-		editarContraseña.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) 
-			{
-				fieldContraseña.setEditable(true);
-				fieldContraseña.selectAll();
-			}
-		});
-		editarContraseña.setBounds(742, 220, 38, 35);
-		contentPane.add(editarContraseña);
+		editarContraseña.addMouseListener(new EditarField(fieldContraseña));
+		
+		editarNombre = new JLabel("");
+		editarNombre.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		editarNombre.addMouseListener(new EditarField(fieldNombre));
+		
+		editarApellido = new JLabel("");
+		editarApellido.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		editarApellido.addMouseListener(new EditarField(fieldApellido));
+		
+		editarEdad = new JLabel("");
+		editarEdad.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		editarEdad.addMouseListener(new EditarField(fieldEdad));
+		
+		editarTelefono = new JLabel("");
+		editarTelefono.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		editarTelefono.addMouseListener(new EditarField(fieldTelefono));
+		
+		editarApellido.setBounds(742, 431, 38, 35);
+		editarEdad.setBounds(742, 535, 38, 35);
+		editarNombre.setBounds(742, 327, 38, 35);
+		editarContraseña.setBounds(742, 220, 38, 35);	
 		editarNombreUsuario.setBounds(743, 117, 38, 35);
+		editarTelefono.setBounds(742, 636, 38, 35);
+		
 		contentPane.add(editarNombreUsuario);
+		contentPane.add(editarTelefono);
+		contentPane.add(editarEdad);
+		contentPane.add(editarApellido);		
+		contentPane.add(editarContraseña);
+		contentPane.add(editarNombre);
 		
 		botonGuardar = new JLabel("");
 		botonGuardar.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
@@ -264,5 +340,49 @@ public class Cuenta extends JFrame {
 		Fondo.setIcon(new ImageIcon(Cuenta.class.getResource("/img/Cuenta.png")));
 		Fondo.setBounds(0, 0, 1294, 721);
 		contentPane.add(Fondo);
+	}
+	
+	class EditarField implements MouseListener
+	{
+
+		JTextField fieldEditar;
+		
+		public EditarField(JTextField fieldEditar)
+		{
+			this.fieldEditar = fieldEditar;
+		}
+		
+		@Override
+		public void mouseClicked(MouseEvent arg0) 
+		{
+			fieldEditar.setEditable(true);			
+			fieldEditar.selectAll();
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+			// TODO Auto-generated method stub
+			
+		}
+		
+		
 	}
 }
--- src/graficas/Productos.java
+++ src/graficas/Productos.java
@@ -2,7 +2,11 @@ package graficas;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Cursor;
 import java.awt.EventQueue;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -10,36 +14,16 @@ import javax.swing.border.BevelBorder;
 import javax.swing.border.EmptyBorder;
 import javax.swing.border.SoftBevelBorder;
 
-import Objetos.Carro;
 import Objetos.Supermercado;
-import Objetos.Usuario;
-import productos.Bebidas;
-import productos.Lacteo;
-import productos.MapaCategoria;
 import productos.MapaProductos;
-import productos.Snack;
-
 import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
-import java.awt.Cursor;
-import javax.swing.border.CompoundBorder;
-import javax.swing.border.MatteBorder;
-import javax.swing.UIManager;
-import java.awt.Font;
-
-public class Inicio extends JFrame {
+public class Productos extends JFrame {
 
 	private JPanel contentPane;
-	int x;
-	int y;
-
+	int x, y;
 	/**
 	 * Launch the application.
 	 */
@@ -47,7 +31,7 @@ public class Inicio extends JFrame {
 //		EventQueue.invokeLater(new Runnable() {
 //			public void run() {
 //				try {
-//					Inicio frame = new Inicio();
+//					Productos frame = new Productos();
 //					frame.setVisible(true);
 //				} catch (Exception e) {
 //					e.printStackTrace();
@@ -59,22 +43,18 @@ public class Inicio extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public Inicio(Supermercado mercado) {
+	public Productos(Supermercado mercado, MapaProductos productos) {
 		
 		setUndecorated(true);
-		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1300, 750);
 		setBackground(new Color(0,0,0,0));
 		setLocationRelativeTo(null);
-
 		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
 		contentPane.setBackground(new Color(0,0,0,0));
-		
+		setContentPane(contentPane);
 		
 		JLabel labelMover = new JLabel("");
 		labelMover.addMouseListener(new MouseAdapter() {
@@ -91,22 +71,35 @@ public class Inicio extends JFrame {
 			}
 		});
 		labelMover.setCursor(Cursor.getPredefinedCursor(Cursor.MOVE_CURSOR));
-		labelMover.setBounds(32, 11, 1233, 35);
+		labelMover.setBounds(43, 30, 1233, 35);
 		contentPane.add(labelMover);
 		
-		JButton btnNewButton = new JButton("X");
-		btnNewButton.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) 
+		JButton botonInicio = new JButton("");
+		botonInicio.setContentAreaFilled(false);
+		botonInicio.setBorder(null);
+		botonInicio.setVisible(true);
+		botonInicio.setOpaque(false);
+		botonInicio.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) 
 			{
-				System.exit(0);
+				botonInicio.setBorder(new SoftBevelBorder(BevelBorder.LOWERED, null, null, null, null));
+			}
+			@Override
+			public void mouseExited(MouseEvent e) 
+			{
+				botonInicio.setBorder(null);
+			}
+			@Override
+			public void mouseClicked(MouseEvent arg0) 
+			{
+				new Inicio(mercado).setVisible(true);
+				setVisible(false);
 			}
 		});
-		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
-		btnNewButton.setBackground(new Color(192, 192, 192));
-		btnNewButton.setForeground(new Color(0, 0, 0));
-		btnNewButton.setBounds(1194, 14, 46, 26);
-		contentPane.add(btnNewButton);
-		
+		contentPane.setLayout(null);
+		botonInicio.setBounds(52, 302, 196, 51);
+		contentPane.add(botonInicio);
 		
 		JButton botonCarro = new JButton("");
 		botonCarro.setVisible(true);
@@ -124,9 +117,10 @@ public class Inicio extends JFrame {
 				botonCarro.setBorder(null);
 			}
 		});
+		contentPane.setLayout(null);
 		
 		botonCarro.setContentAreaFilled(false);
-		botonCarro.setBounds(50, 467, 196, 51);
+		botonCarro.setBounds(53, 363, 196, 51);
 		contentPane.add(botonCarro);
 
 		JButton botonCuenta = new JButton("");
@@ -152,7 +146,7 @@ public class Inicio extends JFrame {
 			}
 		});
 		botonCuenta.setContentAreaFilled(false);
-		botonCuenta.setBounds(50, 407, 196, 51);
+		botonCuenta.setBounds(53, 421, 196, 51);
 		contentPane.add(botonCuenta);
 		
 		JButton botonHistorial = new JButton("");
@@ -173,67 +167,17 @@ public class Inicio extends JFrame {
 		});
 		
 		botonHistorial.setContentAreaFilled(false);
-		botonHistorial.setBounds(50, 347, 196, 51);
+		botonHistorial.setBounds(55, 480, 196, 51);
 		contentPane.add(botonHistorial);
 		
-		JPanel panel = new JPanel();
-		panel.setBorder(null);
-		panel.setOpaque(false);
-		panel.setBounds(254, 40, 1017, 641);
-		contentPane.add(panel);
-		panel.setLayout(null);
 		
-		crearMatrizBotones(panel, mercado);
 		
-		JLabel Fondo = new JLabel("");
-		Fondo.setIcon(new ImageIcon(Inicio.class.getResource("/img/Inicio.png")));
-		Fondo.setBounds(0, 0, 1294, 721);
-		contentPane.add(Fondo);
+		
+		
+		JLabel fondo = new JLabel("");
+		fondo.setBounds(5, 5, 1290, 740);
+		fondo.setIcon(new ImageIcon(Productos.class.getResource("/img/Productos.png")));
+		contentPane.add(fondo);
 	}
-	
-	void crearMatrizBotones(JPanel panel, Supermercado mercado)
-	{
-		int x = 54;
-		int y = 52;
-		JButton arreglo[] = new JButton[mercado.cantidadCategorias()];
-		for(int i = 0; i < mercado.cantidadCategorias(); i++)
-		{
-			arreglo[i] = new JButton(mercado.getNombreCategoria((long) i + 1) + "s");
-			arreglo[i].setBounds(x, y, 192, 40);
-			arreglo[i].setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 15));
-			arreglo[i].setForeground(new Color(255, 255, 255));
-			arreglo[i].setBackground(new Color(0, 102, 255));
-			arreglo[i].setBorder(UIManager.getBorder("CheckBox.border"));
-			arreglo[i].setActionCommand(mercado.getNombreCategoria((long) i + 1));
-			arreglo[i].addActionListener(new Pulsando(arreglo[i], mercado));
-			panel.add(arreglo[i]);
-			x += 192 + 20;
-			
-			if(i == 4) 
-			{
-				x = 54;
-				y += 40 + 20;
-			}
-		}	
-	}
-	
-	class Pulsando implements ActionListener{
-		
-		Supermercado mercado;
-		JButton button;
-		
-		public Pulsando(JButton button, Supermercado mercado) {
-			
-			this.button = button;
-			this.mercado = mercado;
-		}
-		
-		public void actionPerformed(ActionEvent e) {
-	        String actionCommand = ((JButton) e.getSource()).getActionCommand();
-	        MapaProductos productos = mercado.getCategoriaPorNombre(actionCommand);
-	        setVisible(false);
-	        new Productos(mercado, productos).setVisible(true);
-	    }
-		
-	}
+
 }
