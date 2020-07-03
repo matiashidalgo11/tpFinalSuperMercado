a4851664a936595a9123bee1061445cfaff1822c
matiashidalgo11 <matiashidalgo18@gmail.com>
2020-07-03 15:36:57 -0300

    Informacion General

--- src/graficasAdmin/MenuAdminInformacionGeneral.java
+++ src/graficasAdmin/MenuAdminInformacionGeneral.java
@@ -9,15 +9,14 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.SystemColor;
 import javax.swing.border.BevelBorder;
+import javax.swing.ImageIcon;
 
 public class MenuAdminInformacionGeneral extends JPanel {
-	private JLabel lblNewLabel;
-	private JLabel lblNewLabel_1;
 	private JLabel lblCantidadProductos;
 	private JLabel lblCantidadUusarios;
-	private JLabel lblNewLabel_4;
 	private JLabel lblProductosEnOferta;
 	private Supermercado datos;
+	private JLabel lblNewLabel_2;
 
 	/**
 	 * Create the panel.
@@ -34,37 +33,26 @@ public class MenuAdminInformacionGeneral extends JPanel {
 	
 		setBounds(232, 11, 1042, 689);
 		setLayout(null);
-
-	
-		lblNewLabel = new JLabel("Cantidad de Usuarios");
-		lblNewLabel.setFont(new Font("Calibri", Font.BOLD, 23));
-		lblNewLabel.setBounds(50, 103, 330, 82);
-		add(lblNewLabel);
-		
-		lblNewLabel_1 = new JLabel("Cantidad de Productos");
-		lblNewLabel_1.setFont(new Font("Calibri", Font.BOLD, 23));
-		lblNewLabel_1.setBounds(50, 274, 330, 82);
-		add(lblNewLabel_1);
 		
 		lblCantidadProductos = new JLabel("" + datos.cantidadProductos());
 		lblCantidadProductos.setFont(new Font("Calibri", Font.BOLD, 23));
-		lblCantidadProductos.setBounds(520, 274, 330, 82);
+		lblCantidadProductos.setBounds(414, 269, 80, 60);
 		add(lblCantidadProductos);
 		
 		lblCantidadUusarios = new JLabel("" + datos.cantidadUsuarios());
 		lblCantidadUusarios.setFont(new Font("Calibri", Font.BOLD, 23));
-		lblCantidadUusarios.setBounds(520, 103, 330, 82);
+		lblCantidadUusarios.setBounds(414, 136, 80, 60);
 		add(lblCantidadUusarios);
-		
-		lblNewLabel_4 = new JLabel("Productos en Oferta");
-		lblNewLabel_4.setFont(new Font("Calibri", Font.BOLD, 23));
-		lblNewLabel_4.setBounds(50, 420, 330, 82);
-		add(lblNewLabel_4);
 		
 		lblProductosEnOferta = new JLabel("\"\"");
 		lblProductosEnOferta.setFont(new Font("Calibri", Font.BOLD, 23));
-		lblProductosEnOferta.setBounds(520, 420, 330, 82);
+		lblProductosEnOferta.setBounds(414, 401, 80, 60);
 		add(lblProductosEnOferta);
+		
+		lblNewLabel_2 = new JLabel("Fondo");
+		lblNewLabel_2.setIcon(new ImageIcon(MenuAdminInformacionGeneral.class.getResource("/img/Informacion General.png")));
+		lblNewLabel_2.setBounds(-45, 29, 1042, 689);
+		add(lblNewLabel_2);
 	}
 	
 	
